f30a95ce12dbe693247646183b76792fb1e13a1c
steliviere <32293736+steliviere@users.noreply.github.com>
2018-03-19 18:54:23 +0900

    Update sketch.js

--- src/sketch.js
+++ src/sketch.js
@@ -1,18 +1,28 @@
-function setup()
-{
-	createCanvas(window.innerWidth,window.innerHeight);
-	noStroke();
+function setup() {
+  createCanvas(480,480);
+	noFill();
+	stroke(0);
 }
 
-function draw()
-{
-	background(255);
-	fill(255,0,map(mouseX,0,width,0,255));
-	rect(width/2-25,height/2-25,50,50);
-	line(1,1,50,50);
-}
-
-function windowResized()
-{
-	resizeCanvas(window.innerWidth,window.innerHeight);
+function draw() {
+	scale(2);
+  background(255);
+	stroke("#0000ff");
+	rect(10,40,40,40);
+	rect(20,180,80,40);
+	rect(120,80,40,90);
+	rect(150,20,70,70);
+	rect(190,150,30,30);
+	stroke(0);
+	ellipse(100,80,80,80);
+	ellipse(150,20,20,20);
+	ellipse(15,155,10,10);
+	ellipse(105,180,30,30);
+	ellipse(190,190,50,50);
+	stroke("#ff0000");
+	line(35,15,80,60);
+	line(90,35,90+80,35+80);
+	line(30,140,30+50,140+50);
+	line(130,190,130+20,190+20);
+	line(205,30,205+25,30+25);
 }
