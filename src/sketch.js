04a315df2ff37761075de8f2f2d631fcc185eaa9
steliviere <32293736+steliviere@users.noreply.github.com>
2018-03-19 18:19:14 +0900

    Update sketch.js

--- src/sketch.js
+++ src/sketch.js
@@ -1,14 +1,15 @@
 function setup()
 {
 	createCanvas(window.innerWidth,window.innerHeight);
-	fill("#ff0000");
 	noStroke();
 }
 
 function draw()
 {
 	background(255);
+	fill(255,0,map(mouseX,0,width,0,255));
 	rect(width/2-25,height/2-25,50,50);
+	line(1,1,50,50);
 }
 
 function windowResized()
